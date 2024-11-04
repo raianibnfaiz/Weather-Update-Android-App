3fa0ae532c09317a099c07fafa8a86c4db2acce6
Raian Ibn Faiz <raianibnfaiz@gmail.com>
2024-11-04 12:18:43 +0600

    weather screen search box added

--- app/src/main/java/com/raianibnfaiz/weatherupdate/WeatherScreen.kt
+++ app/src/main/java/com/raianibnfaiz/weatherupdate/WeatherScreen.kt
@@ -1,4 +1,49 @@
 package com.raianibnfaiz.weatherupdate
 
-class WeatherScreen {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun WeatherScreen() {
+    var city by remember {
+        mutableStateOf("")
+    }
+
+    Column(
+         modifier = Modifier.fillMaxWidth().padding(16.dp),
+         horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(modifier = Modifier.fillMaxWidth().padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween)   {
+
+                OutlinedTextField(value = city, onValueChange ={
+                    city = it
+                },
+                label = {
+                        Text(text = "City")
+                    }
+                )
+                IconButton(onClick = {}){
+                    Icon(Icons.Filled.Search, contentDescription = "Search")
+                }
+        }
+    }
 }
